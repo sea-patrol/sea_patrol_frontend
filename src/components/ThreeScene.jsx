6793b3570dfd3395cc1efda40b1315c6c9ca6adf
Ivan Kopylov <ivv.kopylov@gmail.com>
2025-09-05 12:41:45 +0300

    added ocean, sky

--- src/components/ThreeScene.jsx
+++ src/components/ThreeScene.jsx
@@ -1,11 +1,14 @@
 import { Canvas } from '@react-three/fiber';
-import { OrbitControls } from '@react-three/drei';
+import { OrbitControls, Sky } from '@react-three/drei';
+import Ocean from './Ocean';
 
 function ThreeScene() {
   return (
-    <Canvas>
+    <Canvas camera={{ position: [0, 5, 100], fov: 55, near: 1, far: 20000 }}>
       <ambientLight intensity={0.5} />
-      <pointLight position={[10, 10, 10]} />
+
+      <Ocean/>
+      <Sky scale={1000} sunPosition={[500, 150, -1000]} turbidity={0.1} />
       <mesh>
         <boxGeometry args={[1, 1, 1]} />
         <meshStandardMaterial color="red" />
