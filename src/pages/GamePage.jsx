c12d2a46d6a4292eba0102b4bf1847c31e138283
Ivan Kopylov <ivv.kopylov@gmail.com>
2025-09-28 22:39:49 +0300

    added username on the top of the game scene

--- src/pages/GamePage.jsx
+++ src/pages/GamePage.jsx
@@ -1,11 +1,17 @@
 import ChatBlock from '../components/ChatBlock';
 import ProfileBlock from '../components/ProfileBlock';
 import GameMainScene from '../components/GameMainScene';
+import { useAuth } from '../contexts/AuthContext';
 import '../styles/GamePage.css'
 
 function GamePage() {
+  const { user } = useAuth();
+
   return (
     <div style={{ width: '100vw', height: '100vh' }}>
+      <div className='username-display'>
+        {user?.username || 'Player'}
+      </div>
       <div className='chat-block'>
         <ChatBlock />
       </div>
